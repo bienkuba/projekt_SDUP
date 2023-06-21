ed21eb1a0e67c5483b78437b6edd8cce52f866d0
bienkuba <bienkuba@interia.pl>
2023-06-21 20:39:12 +0200

    diodes and switches

--- Turbocody/Turbocody.srcs/sources_1/new/check_if_equal.v
+++ Turbocody/Turbocody.srcs/sources_1/new/check_if_equal.v
@@ -1,4 +1,24 @@
 `timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 22.05.2023 14:41:56
+// Design Name: 
+// Module Name: check_if_equal
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
 
 module check_if_equal(
     input clk,
@@ -20,10 +40,25 @@ module check_if_equal(
             led1 <= 0;
             led2 <= 0;
             led3 <= 0;
+        end
+        else begin
+            led0 <= (original == data_decode_1 == data_deinterleaved)   ? 1 : 0; //all outputs correct
+            led1 <= (data_decode_1 == data_deinterleaved)               ? 1 : 0; //original with error
+            led2 <= (original == data_deinterleaved)                    ? 1 : 0; //parity1 with error
+            led3 <= (original == data_decode_1)                         ? 1 : 0; //parity2 with error
+        end
+    end
+    
+/*    always @(posedge clk) begin
+        if (reset) begin
+            led0 <= 0;
+            led1 <= 0;
+            led2 <= 0;
+            led3 <= 0;
             //output_data <= 8'b00000000;
         end
         else begin
-            //if (enable) begin
+            if (enable) begin
                 if (original == data_deinterleaved == data_decode_1) begin
                       $display("ZDEKODOWANO POPRAWNIE");
                       led0 <= 0;
@@ -31,28 +66,28 @@ module check_if_equal(
                       led2 <= 0;
                       led3 <= 1;
                 end
-                if (original != data_decode_1) begin
+                else if (original != data_decode_1) begin
                     $display("B£¥D: dekoder1");
                     led0 <= 0;
                     led1 <= 1;
                     led2 <= 0;
                     led3 <= 0;
                 end
-                if (original != data_deinterleaved) begin
+                else if (original != data_deinterleaved) begin
                     $display("B£¥D: dekoder2");
                     led0 <= 0;
                     led1 <= 0;
                     led2 <= 1;
                     led3 <= 0;
                 end   
-//                if (original == data_deinterleaved == data_decode_1) begin
-//                    $display("ZDEKODOWANO POPRAWNIE");
-//                    led0 <= 0;
-//                    led1 <= 0;
-//                    led2 <= 0;
-//                    led3 <= 1;
-//                end
-              //end
-         end
+                else if (original == data_deinterleaved == data_decode_1) begin
+                    $display("ZDEKODOWANO POPRAWNIE");
+                    led0 <= 0;
+                    led1 <= 0;
+                    led2 <= 0;
+                    led3 <= 1;
+                end
+              end
+         end*/
     end
 endmodule
